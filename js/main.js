86c7bb922638ca00eeb9aac2e297361d987b8277
Rob Brown <rabrown.mail@gmail.com>
2013-03-08 22:13:12 -0500

    Fixing scrolling and animations.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 $(function() {
-	$('.tip').tooltip();
+	var cubeContainer = $('#cubeContainer');
+
+	$('.leftArrow').click(function() {
+		var pageClass = cubeContainer.attr('class');
+		cubeContainer.removeClass( pageClass );
+		var targetPage;
+		if(pageClass == 'showPortfolio') { targetPage = 'showConnect' };
+		if(pageClass == 'showConnect') { targetPage = 'showAbout' };
+		if(pageClass == 'showAbout') { targetPage = 'showPortfolio' };
+        cubeContainer.addClass( targetPage );
+	});
+	$('.rightArrow').click(function() {
+		var pageClass = $('#cubeContainer').attr('class');
+		cubeContainer.removeClass( pageClass );
+		var targetPage;
+		if(pageClass == 'showPortfolio') { targetPage = 'showAbout' };
+		if(pageClass == 'showAbout') { targetPage = 'showConnect' };
+		if(pageClass == 'showConnect') { targetPage = 'showPortfolio' };
+        cubeContainer.addClass( targetPage );
+	});
+	$('.bottomArrow').click(function() {
+		var pageClass = $('#cubeContainer').attr('class');
+		cubeContainer.removeClass( pageClass );
+		var targetPage;
+		if(pageClass == 'showPortfolio') { targetPage = 'showResume' };
+		if(pageClass == 'showResume') { targetPage = 'showPortfolio' };
+        cubeContainer.addClass( targetPage );
+	});
 });
-
-
