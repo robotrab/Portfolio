a422e7ed48c0b664aac56fed254d23aa8b5e401b
Rob Brown <rabrown.mail@gmail.com>
2013-03-09 08:20:19 -0500

    Fixed arrow animations.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
+var targetDeg = 0;
+var targetBottom = 0;
+
 $(function() {
+	function getRotationDegrees(obj) {
+	    var matrix = obj.css("-webkit-transform") ||
+	    obj.css("-moz-transform")    ||
+	    obj.css("-ms-transform")     ||
+	    obj.css("-o-transform")      ||
+	    obj.css("transform");
+	    if(matrix !== 'none') {
+	        var values = matrix.split('(')[1].split(')')[0].split(',');
+	        var a = values[0];
+	        var b = values[1];
+	        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+	    } else { var angle = 0; }
+	    return angle;
+	}
+
 	var cubeContainer = $('#cubeContainer');
 
 	$('.leftArrow').click(function() {
-		var pageClass = cubeContainer.attr('class');
-		cubeContainer.removeClass( pageClass );
-		var targetPage;
-		if(pageClass == 'showPortfolio') { targetPage = 'showConnect' };
-		if(pageClass == 'showConnect') { targetPage = 'showAbout' };
-		if(pageClass == 'showAbout') { targetPage = 'showPortfolio' };
-        cubeContainer.addClass( targetPage );
+  		var pageClass = cubeContainer.attr('class');
+  		if(targetDeg == 90 || targetDeg%360 == 90 || targetDeg%360 == -270) {
+  			targetDeg += 180;
+  		}
+  		else {
+  			targetDeg += 90;
+  		}
+  		console.log(targetDeg);
+  		$('.'+pageClass).css('-webkit-transform', 'rotateY('+targetDeg+'deg)');
 	});
 	$('.rightArrow').click(function() {
-		var pageClass = $('#cubeContainer').attr('class');
-		cubeContainer.removeClass( pageClass );
-		var targetPage;
-		if(pageClass == 'showPortfolio') { targetPage = 'showAbout' };
-		if(pageClass == 'showAbout') { targetPage = 'showConnect' };
-		if(pageClass == 'showConnect') { targetPage = 'showPortfolio' };
-        cubeContainer.addClass( targetPage );
+		var pageClass = cubeContainer.attr('class');
+  		if(targetDeg == -90 || targetDeg%360 == -90 || targetDeg%360 == 270) {
+  			targetDeg -= 180;
+  		}
+  		else {
+  			targetDeg -= 90;
+  		}
+  		console.log(targetDeg);
+  		$('.'+pageClass).css('-webkit-transform', 'rotateY('+targetDeg+'deg)');
 	});
 	$('.bottomArrow').click(function() {
-		var pageClass = $('#cubeContainer').attr('class');
-		cubeContainer.removeClass( pageClass );
-		var targetPage;
-		if(pageClass == 'showPortfolio') { targetPage = 'showResume' };
-		if(pageClass == 'showResume') { targetPage = 'showPortfolio' };
-        cubeContainer.addClass( targetPage );
+		var pageClass = cubeContainer.attr('class');
+  		if(targetBottom == 90 || targetBottom%360 == 90 || targetBottom%360 == -270) {
+  			targetBottom += 270;
+  		}
+  		else {
+  			targetBottom += 90;
+  		}
+  		console.log(targetBottom);
+  		$('.'+pageClass).css('-webkit-transform', 'rotateX('+targetBottom+'deg)');
 	});
 });
